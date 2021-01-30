d98e35d3ea2aaa35c831fc1ff451b4658a4208f2
visualmoney <wonho2023>
2021-01-30 10:51:53 +0900

    Add initial files Visual Cash Android

---
+++ app/src/main/java/org/vimoney/vxcash/android/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,31 @@
+package org.vimoney.vxcash.android.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import org.vimoney.vxcash.android.R
+
+class SlideshowFragment : Fragment() {
+
+    private lateinit var slideshowViewModel: SlideshowViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        slideshowViewModel =
+                ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val textView: TextView = root.findViewById(R.id.text_slideshow)
+        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
